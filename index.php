eded6631b8a5d6bc511398a4512823877e65d399
IDEBeta21 <deladiaian96@gmail.com>
2021-08-08 23:00:43 +0800

    change in index

--- index.php
+++ index.php
@@ -7,14 +7,9 @@
     <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
-    <h1>Hello World!!</h1>
-    <h2> </h2>
-=======
     <h1>Hello World</h1>
     <p> Haluuu </p>
 
     <h1>OOHHHHHH!!!!!!!!</h1>
->>>>>>> 773d1c0fbfc65e0d11a7811db50d3cbe6fd0c1de
 </body>
 </html>
