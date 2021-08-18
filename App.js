0170f6a01342a9c8e6c5a2c2a67a37a272530ea8
IDEBeta21 <deladiaian96@gmail.com>
2021-08-18 15:55:44 +0800

    Add tests for Fetch and Flatlist

--- App.js
+++ App.js
@@ -1,67 +1,117 @@
-import React from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View , Image , TouchableOpacity, Alert, ScrollView} from 'react-native';
-import Header from './components/header'
+import React  from 'react';
+import { useState, useEffect } from 'react';
+import { 
+    KeyboardAvoidingView, StyleSheet, Text, 
+    TextInput, View , Image , 
+    TouchableOpacity, Alert, ScrollView, SafeAreaView, FlatList
+} from 'react-native';
+import Header from './components/header';
 
 // Functions
 const hello = () => {
     Alert.alert('hello world!!');
 }
 
+//Test screen
+{/* This contains the test for using fetch in PHP */}
+const testScreen = () => {
+    //Testing for fetch
+    /* 
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch('http://localhost/rn-php-test/test.php')
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => alert(error + '\nhello world'))
+        .finally(setLoading(false));
+    });
+    */
+
+    //Testing for flatlist with array
+    const [data , setData] = useState([
+        {ComplainantID: '1' , ComplainantName: 'Ian'},
+        {ComplainantID: '2' , ComplainantName: 'Bella'}
+    ])
+    const [isLoading, setLoading] = useState(false);
+
+    return(
+        <SafeAreaView style={({marginTop: 50, paddingHorizontal: 15,})}>
+            {
+                isLoading ? 
+                    (<Text>Hello</Text>) : 
+                    (<FlatList
+                        data = {data}
+                        renderItem={({ item }) => (
+                            <View style={styles.buttonArea}>
+                                <Text style={({padding: 10, color: 'white'})}>{item.ComplainantName}</Text>
+                            </View>
+                        ) } 
+                    />)
+            }
+        </SafeAreaView>
+
+    );
+};
+
 // Layouts
-const MainScreen = () => (
-    <View style={styles.loginContainer}>
-        {/* Display Header */}
-        <Header/>
+const MainScreen = () => {
+    return(
+        <View style={styles.loginContainer}>
+            {/* Display Header */}
+            <Header/>
 
-        {/* Make the view scrollable */}
-        <ScrollView
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-        >
-
-            {/* To detect virtual keyboard */}
-            <KeyboardAvoidingView
-                behavior={Platform.OS === "android" ? "padding" : "height"}
+            {/* Make the view scrollable */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
             >
 
-                {/* Logo area */}
-                <View style={styles.logoView}>
-                    <Image
-                        style = {styles.logo}
-                        source={{
-                            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
-                        }}
-                    />
-                    <Text style={styles.headerText}>PHYTOFLEX</Text>
-                </View>
+                {/* To detect virtual keyboard */}
+                <KeyboardAvoidingView
+                    //behavior={Platform.OS === "android" ? "padding" : "height"}
+                >
 
-                <View style={({paddingHorizontal: 30})}> 
-                    {/* Text Input Area */}
-                    <Text style={styles.label}>Username</Text>
-                    <TextInput
-                        style={styles.textbox}
-                        placeholder="i.e. NameIsDev21"
-                    ></TextInput>
+                    {/* Logo area */}
+                    <View style={styles.logoView}>
+                        <Image
+                            style = {styles.logo}
+                            source={{
+                                uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+                            }}
+                        />
+                        <Text style={styles.headerText}>For Plantitios and </Text>
+                        <Text style={styles.headerText}>Plantitas</Text>
+                    </View>
 
-                    <Text style={styles.label}>Password</Text>
-                    <TextInput
-                        style={styles.textbox}
-                        placeholder="Password"
-                    ></TextInput>
+                    <View style={({paddingHorizontal: 30})}> 
+                        {/* Text Input Area */}
+                        <Text style={styles.label}>Username</Text>
+                        <TextInput
+                            style={styles.textbox}
+                            placeholder="i.e. NameIsDev21"
+                        ></TextInput>
 
-                    {/* Button Area */}
-                    <TouchableOpacity onPress={hello}>
-                        <View style={styles.buttonArea}>
-                            <Text style={{ color: 'white', fontSize: 20, }}>LOGIN</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                
-            </KeyboardAvoidingView>
-        </ScrollView>
-    </View>
-    
-);
+                        <Text style={styles.label}>Password</Text>
+                        <TextInput
+                            style={styles.textbox}
+                            placeholder="Password"
+                        ></TextInput>
+
+                        {/* Button Area */}
+                        <TouchableOpacity onPress={hello}>
+                            <View style={styles.buttonArea}>
+                                <Text style={{ color: 'white', fontSize: 20, }}>LOGIN</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </View>
+    ); 
+}
 
 // Styles
 //Login form Styles
@@ -71,21 +121,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         marginVertical: 0 ,
         borderRadius: 12,
-        marginTop: 0,
     },
     logoView: {
         alignItems: 'center',
         justifyContent: 'center',
-        padding: 50,
+        paddingBottom: 30
     },
     headerText: {
         fontSize: 30,
-        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     textbox: {
         borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 10,
+        borderBottomWidth: 1,
         padding: 10,
         fontSize: 15,
     },
@@ -99,12 +148,24 @@ const styles = StyleSheet.create({
     },
     buttonArea: {
         marginTop: 40,
+        padding: 15,
+
         backgroundColor: 'green',
+        borderRadius: 15,
+
         alignItems: 'center', 
         justifyContent: 'center',
-        borderRadius: 15,
-        padding: 15,
-    }
+        
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 1,
+        elevation: 5,
+    },
 });
 
-export default MainScreen;
+// export default MainScreen;
+export default testScreen ;
--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Header() {
     return(
         <View style={styles.header}>
-            <Text style={({fontSize:30})}>Phytoflex</Text>
+            <Text style={styles.headerText}>PHYTOFLEX</Text>
         </View>
     )
 }
@@ -14,6 +14,22 @@ const styles = StyleSheet.create({
         height: 100, 
         paddingTop: 38,
         backgroundColor: 'green',
-        paddingLeft: 15
+        paddingLeft: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        //Render shadow
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 5,
+        elevation: 15,
     },
+    headerText: {
+        fontSize: 30 ,
+        fontWeight: 'normal' 
+    }
 });
