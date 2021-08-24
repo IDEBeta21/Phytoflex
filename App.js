584db71f55aecf9e01855c61cb5d66004aba6a17
Leila Jane Alejandre <leilaalejandre@gmail.com>
2021-08-24 11:17:49 +0800

    Update App.js

--- App.js
+++ App.js
@@ -67,8 +67,8 @@ function loginScreen({ navigation }){
                 uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
               }}
             />
-            <Text style={styles.headerText}>For Plantitos and </Text>
-            <Text style={styles.headerText}>Plantitas</Text>
+            <Text style={styles.headerText}>For Plantitos </Text>
+            <Text style={styles.headerText}> and Plantitas</Text>
           </View>
           
           <View style={({paddingHorizontal: 30})}> 
