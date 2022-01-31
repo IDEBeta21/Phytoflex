6d6b3256e757721e4f8009d566938194d645734c
IDEBeta21 <kitsmype416@gmail.com>
2022-01-31 11:10:07 +0800

    Navigation Test
    Check the js files in screens/plantcare

--- screens/plantcare/mainPlantCare.js
+++ screens/plantcare/mainPlantCare.js
@@ -1,59 +1,80 @@
-import * as React from "react";
-import {ScrollView, View, Text, StyleSheet, Button} from 'react-native';
-import { globalStyles } from '../global/globalStyles';
+// import * as React from "react";
+// import {ScrollView, View, Text, StyleSheet, Button} from 'react-native';
+// import { globalStyles } from '../global/globalStyles';
 
-// Imports for react navigation
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+// // Imports for react navigation
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { NavigationContainer } from '@react-navigation/native';
+// import LoginScreen from "../landing/login";
 
 import PlantCareHomePage from "../../screens/plantcare/plantCareHomePage";
 import PlantCareSearchPage from "./plantCareSearch";
 import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from 'react-navigation';
 
-function PlantCareHomeScreen({ navigation }) {
-  return (
-    <PlantCareHomePage navigation={navigation}/>
-  );
+// const Drawer = createDrawerNavigator();
+const PlantCareScreen = {
+  PlantCareHome: {
+    screen: PlantCareHomePage,
+    navigationOptions:{
+      headerShown: false
+    }
+  },
+  PlantCareSearch: {
+    screen: PlantCareSearchPage
+  }
 }
 
-function PlantCareSearhScreen({ navigation }) {
-  return (
-    <PlantCareSearchPage navigation={navigation}/>
-  );
-}
+const PlantCareStack = createStackNavigator(PlantCareScreen);
 
-const Drawer = createDrawerNavigator();
-// const PlantCareStack = createStackNavigator();
+export default createAppContainer(PlantCareStack);
 
-export default function PlantCare(){
+
+
+// function PlantCareHomeScreen({ navigation }) {
+//   return (
+//     <PlantCareHomePage navigation={navigation}/>
+//   );
+// }
+
+// function PlantCareSearhScreen({ navigation }) {
+//   return (
+//     <PlantCareSearchPage navigation={navigation} title="Plant care"/>
+//   );
+// }
+
+// export default createStackNavigator(PlantCareScreen);
+// export default function PlantCare(){
+  
+//   return(
+//     // <PlantCareStack.Navigator 
+//     //   initialRouteName="Plant Care"
+//     //   screenOptions={{
+//     //     // drawerStyle:{
+//     //     //   backgroundColor: '#1D4123',
+//     //     // },
+//     //     // drawerInactiveTintColor: 'grey',
+//     //     // drawerActiveTintColor: 'white',
+//     //     headerShown: false
+//     //   }}>
+//     //   <PlantCareStack.Screen name="Plant Care" component={PlantCareHomeScreen} />
+//     //   <PlantCareStack.Screen name="Search" component={PlantCareSearhScreen} />
+//     //   <PlantCareStack.Screen name="Search" component={LoginScreen} />
+//     // </PlantCareStack.Navigator>
     
-  return(
-    // <PlantCareStack.Navigator 
-    //   initialRouteName="Plant Care"
-    //   screenOptions={{
-    //     // drawerStyle:{
-    //     //   backgroundColor: '#1D4123',
-    //     // },
-    //     // drawerInactiveTintColor: 'grey',
-    //     // drawerActiveTintColor: 'white',
-    //     headerShown: false
-    //   }}>
-    //   <PlantCareStack.Screen name="Plant Care" component={PlantCareHomeScreen} />
-    //   <PlantCareStack.Screen name="Search" component={PlantCareSearhScreen} />
-    // </PlantCareStack.Navigator>
-    
 
-    <Drawer.Navigator 
-      initialRouteName="Home"
-      screenOptions={{
-        drawerStyle:{
-          backgroundColor: '#1D4123',
-        },
-        drawerInactiveTintColor: 'grey',
-        drawerActiveTintColor: 'white',
-      }}>
-      <Drawer.Screen name="PlantCare" component={PlantCareHomeScreen} />
-      <Drawer.Screen name="Search" component={PlantCareSearhScreen} />
-    </Drawer.Navigator>
-  );
-}
+//     // <Drawer.Navigator 
+//     //   initialRouteName="Home"
+//     //   screenOptions={{
+//     //     drawerStyle:{
+//     //       backgroundColor: '#1D4123',
+//     //     },
+//     //     drawerInactiveTintColor: 'grey',
+//     //     drawerActiveTintColor: 'white',
+//     //     headerShown: false
+//     //   }}>
+//     //   <Drawer.Screen name="PlantCare" component={PlantCareHomeScreen} />
+//     //   <Drawer.Screen name="Search" component={PlantCareSearhScreen} />
+//     // </Drawer.Navigator>
+//   );
+// }
--- screens/plantcare/plantCareHomePage.js
+++ screens/plantcare/plantCareHomePage.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 import { globalStyles } from '../global/globalStyles';
 
 export default function PlantCareHomePage({navigation}) {
-    return (
-      <View style={ globalStyles.textContainer }>
-        <Text style={ globalStyles.titleText }>
-          This is the Plant Care Main Page
-        </Text>
-        <Text style={ globalStyles.paragraphText }>
-          Open up plantCareHomePage.js to start working on your app!
-        </Text>
+  // Calling Plantcare search screen
+  const toPlantCareSearch = () => {
+    navigation.navigate('PlantCareSearch');
+  }
 
-        <Button onPress={() => navigation.push('PlantCareHomeScreen')} title="Click"></Button>
-      </View>
-    );
+  return (
+    <View style={ globalStyles.textContainer }>
+      <Text style={ globalStyles.titleText }>
+        This is the Plant Care Main Page
+      </Text>
+      <Text style={ globalStyles.paragraphText }>
+        Open up plantCareHomePage.js to start working on your app!
+      </Text>
+
+      <Button onPress={() => toPlantCareSearch()} title="Click"></Button>
+    </View>
+  );
 }
