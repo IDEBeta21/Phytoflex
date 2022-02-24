dca86158ab1ffd55caffa119c313d788ed77f602
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2022-02-25 01:26:19 +0800

    Update plantCareHealthCare.js
    Fixed modal import

--- screens/plantcare/plantCareHealthCare.js
+++ screens/plantcare/plantCareHealthCare.js
@@ -5,7 +5,7 @@ import { globalStyles } from '../global/globalStyles';
 import { DrawerContent } from '../global/Drawer';
 
 import firebase from 'firebase';
-import PFModalLogin from '../../components/Modals/PFModalLogin';
+import {PFModalLogin} from '../../components/Modals';
 
 export default function PlantCareHealtCarePage({navigation}) {
   // Calling Plantcare search screen
