46d5ae3bd362b7d005b3d8c97454ce0fda2e02c9
pammytupas <88882634+pammytupas@users.noreply.github.com>
2022-05-07 22:51:00 +0800

    Revert "ActivityLogScreen"
    This reverts commit 9187a424aacfa5b0b632b78891e055260ce8cd59.

--- screens/DrawerContents/ActivityLogs.js
+++ screens/DrawerContents/ActivityLogs.js
@@ -1,45 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Button, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { globalStyles } from '../global/globalStyles';
 
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import PostsLog from './ActivityLogScreens/PostsLog';
-import ThreadsLog from './ActivityLogScreens/ThreadsLog';
+import { DrawerContent } from '../global/Drawer';
 
-function FuncPostsLog({navigation}){
-  return(
-    <PostsLog navigation={navigation}/>
-  )
-}
-function FuncThreadsLog({navigation}){
-  return(
-    <ThreadsLog navigation={navigation}/>
-  )
-}
-
-const Tab = createMaterialTopTabNavigator();
-
-export default function ActivityLogsScreen() {
+export default function ActivityLogsScreen({navigation}) {
+  
   return (
-    <Tab.Navigator
-    initialRouteName="PostsLog"
-    screenOptions={{
-      tabBarActiveTintColor: '#1D4123',
-      tabBarInactiveTintColor: '#f5f5f5',
-      tabBarLabelStyle: { fontSize: 13, fontFamily: 'poppins-regular', textTransform: 'capitalize' },
-      tabBarStyle: { backgroundColor: '#1D4123', elevation:0  },
-      tabBarIndicatorStyle: {backgroundColor: '#f2f2f2',  borderColor: '#1D4123', height: '100%'}
-    }}  
-    >
-    <Tab.Screen
-      name="PostsLog"
-      component={FuncPostsLog}
-      options={{ tabBarLabel: 'Posts' }}
-    />
-    <Tab.Screen
-      name="ThreadsLog"
-      component={FuncThreadsLog}
-      options={{ tabBarLabel: 'Threads' }}
-    />
-    </Tab.Navigator>
-  )
+    <View style={ globalStyles.textContainer }>
+      <Text style={ globalStyles.titleText }>
+            ActivityLogs </Text>
+      <Text style={ globalStyles.titleText }>
+            Yown Gumana din hahaha      </Text>
+      <Text style={ globalStyles.paragraphText }>
+            Hello Hello Testing      </Text>
+    </View>
+  );
 }
