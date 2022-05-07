9187a424aacfa5b0b632b78891e055260ce8cd59
Leila Jane Alejandre <leilaalejandre@gmail.com>
2022-05-07 21:40:41 +0800

    ActivityLogScreen

---
+++ screens/DrawerContents/ActivityLogScreens/ThreadsLog.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+
+export default function ThreadsLog({navigation}) {
+  return (
+    <View>
+    <StatusBar style="auto" />
+      <Text>ThreadsLogs</Text>
+    </View>
+  )
+}
