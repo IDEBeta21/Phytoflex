e31a0294083301fa410f782ed24cabf01bcff88b
pammytupas <88882634+pammytupas@users.noreply.github.com>
2022-05-11 12:03:03 +0800

    update createQuestion.js

--- screens/discussion/createQuestion.js
+++ screens/discussion/createQuestion.js
@@ -1,10 +1,11 @@
 import { Button, Text, View, StyleSheet, TextInput, Image, TouchableOpacity, Alert, FlatList, Pressable, ViewPropTypes} from 'react-native';
-import React, { Component, useState } from 'react';
+import React,  { Component, useState, useEffect } from 'react';
 import { Portal } from 'react-native-paper';
 
 import { globalStyles } from '../global/globalStyles';
 
 import { NavigationContainer } from '@react-navigation/native';
+import firebase from 'firebase';
 
 import { 
   PFText , PFTextInput, PFPopupMenu, PFActivityIndicator,
@@ -24,6 +25,54 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 
 export default function CreateQuestionPage({navigation}) {
+  const [refdata2, setrefdata2] = useState([]); // declaration 
+  const [refnull2, setrefnull2] = useState(true);
+
+  //get user info
+
+  const getUsers = async() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User logged in already or has just logged in.
+        console.log(user.email);
+        firebase.firestore()
+    .collection('users').where("userEmail", "==", user.email).get().then((snapshot) => {
+      let users = snapshot.docs.map(doc => { 
+        const data2 = doc.data();
+        const id2 = doc.id;
+        return {id2, ...data2}
+      })
+      setrefdata2(users);
+      console.log(refdata2);
+      setrefnull2(false);
+    }).catch((err) => {
+      Alert.alert(err)
+    }) 
+      } else {
+        // User not logged in or has just logged out.
+      }
+    });
+            // Get data inside document
+    
+  }
+  useEffect(() => {
+  
+    getUsers();
+}, []);
+   
+  //display userImage from firebase
+  const [image, setimage] = useState(null)
+  let profilePic = "";
+  let userName = "";
+  refdata2.forEach((item) => {
+   profilePic = item.profilePic
+   userName = item.userName
+  });
+
+  firebase.storage().ref().child(profilePic).getDownloadURL().then((url) => {
+    setimage(url);
+    })
+ 
 
   return (
     <View style={ styles.mainContainer }>
@@ -31,13 +80,13 @@ export default function CreateQuestionPage({navigation}) {
         <Image
           // FAB using TouchableOpacity with an image
           // For online image
-          source={ require('../../assets/img/profiles/Alejandre.jpg')}
+          source={{uri : image}}
           // For local image
           //source={require('./images/float-add-icon.png')}
           style={styles.userPhoto}
         />
         <View styles={{flexDirection: 'column'}}>
-          <PFText weight='semi-bold' size={15} style={{marginLeft: 10}}>@leyy</PFText>
+          <PFText weight='semi-bold' size={15} style={{marginLeft: 10}}>@{userName}</PFText>
           <PFText size={11} style={{marginLeft: 10}}>Public //pending part</PFText>
         </View>
       </View>
